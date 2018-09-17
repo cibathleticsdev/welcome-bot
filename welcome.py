6c625e1fcc80d013f38265f831131ede5b6106c4
viktaur <30535579+viktaur@users.noreply.github.com>
2018-09-17 18:34:50 +0200

    U

--- welcome.py
+++ welcome.py
@@ -12,7 +12,7 @@ import ctx
 from weather import Weather, Unit #pip install weather-api / pip3 install weather-api
 import time #pip install time / pip3 install time
 from datetime import datetime
-import pytz
+import pytz #pip install pytz / pip3 install pytz
 import random
 import site
 import sys #pip install sys / pip3 install sys
@@ -24,7 +24,7 @@ client = discord.Client()
 @client.event
 async def on_member_join(member):
     print("Recognised that a member called " + member.name + " joined")
-    await client.send_message(member, 'Welcome to this server, ' + member.mention + '! I\'m Archie, the official CADevelopers discord bot, nice to meet you! Please, read our #rules and #about in our discord server https://discord.gg/TS583KK to be updated. To see the list of commands type !help. Have fun and RESPECT!! :grinning: :desktop:')
+    await client.send_message(member, 'Welcome to this server, ' + member.mention + '! I\'m Welcome Bot, a CAD bot and assistant of Archie, the official CADevelopers discord bot. Nice to meet you! Please, read our #rules and #about in our discord server https://discord.gg/TS583KK to be updated. To see Archie\'s list of commands type !help. Have fun and RESPECT!! :grinning: :desktop:')
     # await client.send_message(message.channel, 'Welcome to this server, @' + member.name + '! I\'m Archie, the official CADevelopers discord bot, nice to meet you! Please, read our #rules and #about in our discord server https://discord.gg/TS583KK to be updated. To see the list of commands type !help. Have fun and RESPECT!! :grinning: :desktop:')
     print("Message sent to " + member.name)
 
@@ -34,8 +34,8 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name="CAD Developers | !help"))
-    await client.send_message(discord.Object(id='481951758722138113'), 'Archie is now online! Type !help for more info. Enjoy!')
+    await client.change_presence(game=discord.Game(name="CADevelopers | !help"))
+    await client.send_message(discord.Object(id='481951758722138113'), 'Welcome Bot is now online!')
 
 if __name__ == '__main__':
     import config
